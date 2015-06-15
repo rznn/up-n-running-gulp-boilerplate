e3d6ed6ca48a5c15bb8911d75be0fbe2f6c6981d
Razvan Negrea <rznart@gmail.com>
2015-06-15 15:43:08 +0100

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Up n' running Gulp Boilerplate
+
+## Welcome
+
+Hello! If you are a **front-end developer** looking to get your project started in a matter of minutes, take a look ahead.
+
+As the name suggests, this boilerplate requires minimum to no additional configuration and features all the essentials you will ever need for a simple project: **a webserver**, **SASS**, **bourbon/neat** (the light-weight alternative to Compass), **JS-hint**, **plumber**, **sourcemaps**, **browser-sync** and more.
+
+* [Installing](#installing)
+* [Webserver and Browser-Sync](#webserver-and-browser-sync)
+* [Structure](#structure)
+  * [Packages](#packages)
+  * [Folder structure](#folder-structure)
+* [FAQ](#faq)
+* [Problems](#problems)
+* [License](#license)
+
+Projects based on this boilerplate: http://rznart.com/work/loremip-uni/
+
+##Installing
+```
+1. Clone the repo
+        git clone https://github.com/rznn/up-n-running-gulp-boilerplate.git
+2. Install dependencies
+        npm install
+3.Start gulp
+        gulp
+4. Your preferred browser will be opened automatically and directed to the browser-sync proxy address
+5. For production, run
+        gulp --production
+???. Have fun!
+```
+
+##Webserver and Browser-Sync
+To start the webserver, simply go to your project directory and run "gulp". The server's default address is http://localhost:3000/.
+
+For debugging via Browser-Sync, go to http://localhost:3001/.
+
+##Structure
+
+###Packages
+* Gulp
+  * [gulp](https://github.com/gulpjs/gulp)
+* CSS/SASS
+  * [sass](https://github.com/sass/sass)
+  * [bourbon](https://github.com/thoughtbot/bourbon)
+  * [neat](https://github.com/thoughtbot/neat)
+  * [autoprefixer](https://github.com/postcss/autoprefixer)
+  * [minify-css](https://github.com/murphydanger/gulp-minify-css)
+* Javascript
+  * [jsHint](https://github.com/jshint/jshint)
+  * [uglify](https://github.com/mishoo/UglifyJS)
+* Images
+  * [imagemin](https://github.com/imagemin/imagemin)
+* MISC
+  * [if](https://github.com/robrich/gulp-if)
+  * [notify](https://github.com/mikaelbr/gulp-notify)
+  * [plumber](https://github.com/floatdrop/gulp-plumber)
+  * [rename](https://github.com/hparra/gulp-rename)
+  * [size](https://github.com/sindresorhus/gulp-size)
+  * [sourcemaps](https://github.com/floridoo/gulp-sourcemaps)
+  * [util](https://github.com/gulpjs/gulp-util)
+  * [yargs](https://github.com/bcoe/yargs)
+
+###Folder Structure
+---
+
+  ```
+  |++ public
+      |++ assets
+          |++ css
+          |++ images
+          |++ js
+  |++ source
+      |-- index.html
+      |++ assets
+          |++ css
+              |-- normalize.css
+          |++ images
+          |++ js
+              |-- functions.js
+          |++ sass
+              |-- main.scss
+              |-- style.scss
+  |-- gulpfile.js
+  |-- LICENSE.txt
+  |-- package.json
+  |-- README.md
+  ```
+##FAQ
+1. **Why Gulp and not Grunt?** - Simple. Because it's faster, way faster. Compilling and launching a browser reload/inject in Grunt ends up taking even 8s in a big project, with Gulp you'll probably end up with a max of 200ms. That, and a personal preference for the more intuitive way in which you configure Gulp.
+
+##Problems
+If you run into any issues whatsoever, do let me know.
+
+This is my first github contribution made as a *HUGE* **thanks to the community**. I expect some things to not be perfect.
+  
+##License
+This boilerplate is licensed under MIT. For more details, please see [LICENSE.txt](https://github.com/rznn/up-n-running-gulp-boilerplate/license.txt).
---
+++ gulpfile.js
@@ -0,0 +1,155 @@
+// -------------------------------------------------------------------------
+// GET THINGS SET UP
+// -------------------------------------------------------------------------
+
+// Include Gulp
+var gulp = require('gulp'); 
+
+// Require webserver
+var webserver = require('gulp-webserver');
+
+// Error handling
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var plumberErrorHandler = { errorHandler: notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+  })
+};
+
+// General plugins
+var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
+var argv = require('yargs').argv;
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Setting up Browser Sync
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// CSS plugins
+var sass = require('gulp-sass');
+var neat = require('node-neat');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+
+// JS plugins
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+
+// Image plugins
+var imagemin = require('gulp-imagemin');
+var size = require('gulp-size');
+
+// -------------------------------------------------------------------------
+// TASKS
+// -------------------------------------------------------------------------
+
+// Start webserver
+gulp.task('webserver', function() {
+  gulp.src('public')
+    .pipe(webserver({
+      livereload: false, // We use Browser Sync, the super-charged brother of Live Reload
+      directoryListing: true,
+      open: true
+    }));
+});
+
+// Create a welcome task and just log a message
+gulp.task('welcome', function() {
+  return gutil.log('Gulp is running!')
+        .beep();
+});
+
+
+// HTML tasks
+gulp.task('html', function() {
+    return gulp.src('source/*.html')
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(gulp.dest('public'));
+});
+
+// CSS tasks
+
+// Copy any CSS files in the source directory to public directory
+gulp.task('css', function () {
+    return gulp.src('source/css/**/*.*')
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+gulp.task('sass', function() {
+    return gulp.src('source/sass/**/*.*')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sourcemaps.init())  // Process the original sources
+            .pipe(sass({
+                includePaths: require('node-neat').includePaths.concat(neat)
+            }))
+            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+            
+            //only uglify if gulp is ran with '--production'
+            .pipe(gulpif(argv.production, minifycss({keepSpecialComments: 0})))
+            .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+
+        .pipe(sourcemaps.write()) // Add the map to modified source.
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+// JS tasks
+gulp.task('lint', function() {
+    return gulp.src('source/js/**/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('source/js/**/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sourcemaps.init())
+            .pipe(concat('all.js'))
+
+            //only uglify if gulp is ran with '--production'
+            .pipe(gulpif(argv.production, uglify()))
+            .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/assets/js'));
+});
+
+// Image tasks
+gulp.task('images', function() {
+    return gulp.src('source/images/**/*.{jpg,jpeg,png,gif}')
+    .pipe(plumber(plumberErrorHandler))
+        .pipe(gulpif(argv.production, imagemin
+            ({
+                optimizationLevel: 3,
+                progessive: true,
+                interlaced: true
+            })
+        ))
+
+        .pipe(gulp.dest('public/assets/images'))
+        .pipe(size());
+});
+
+// Watch files for changes
+gulp.task('watch', function() {
+    gulp.watch('source/*.html', ['html']);
+    gulp.watch('source/sass/**/*.scss', ['sass']);
+    gulp.watch('source/js/**/*.js', ['lint', 'scripts']);
+    gulp.watch('source/images/**/*.{jpg,jpeg,png,gif}', ['images']);
+});
+
+// Sync browser with the changes
+gulp.task('browser-sync', function() {  
+    browserSync.init(['./public/**/*.*'], {
+        server: {
+            baseDir: "./public"
+        }
+    });
+});
+
+// Default Task
+gulp.task('default', ['webserver', 'welcome', 'html', 'css', 'sass', 'lint', 'scripts', 'images', 'watch', 'browser-sync'], reload);
